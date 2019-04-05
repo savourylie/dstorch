90069db9a562c16c599fe7c8e55a3a3b659f99a7
Calvin Ku <calvin.j.ku@googlemail.com>
2019-04-05 22:30:42 +0800

    Updated pypi package to 0.0.7

--- build/lib/dstorch/data/__init__.py
+++ build/lib/dstorch/data/__init__.py
@@ -1,1 +1,3 @@
+from .augmentation import Cutout
+from .preprocessing import int2onehot
 from .stats import calc_data_stats
--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='dstorch',
-      version='0.0.5',
+      version='0.0.7',
       description='A humble package for algorithms and data, implemented in PyTorch.',
    	  package_dir = {
-            'data': 'data'
+            'data': 'data',
+            'utils': 'utils'
             },
       packages=find_packages(),
       url='http://github.com/savourylie/dstorch',
